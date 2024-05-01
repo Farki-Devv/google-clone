c4fffaae87283dc0e5ab099a7b52bf3c1269673b
Your Name <you@example.com>
2024-05-01 22:12:14 +0300

    succesfully

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -3,8 +3,10 @@ import Error from '../error'
 import WebSearchResult from '../../../components/web-search-result'
 
 export default async function Page({ searchParams }) {
+	const startIndex = searchParams.start || '1'
+	await new Promise(resolve => setTimeout(resolve, 1000))
 	const response = await fetch(
-		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
 	)
 	if (!response.ok) throw new Error('Failed to fetch data')
 	const data = await response.json()
@@ -12,4 +14,3 @@ export default async function Page({ searchParams }) {
 	if (!results) return <Error />
 	return <div>{results && <WebSearchResult results={data} />}</div>
 }
-
